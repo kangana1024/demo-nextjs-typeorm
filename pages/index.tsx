ef95653b484a58b79aa898fc79de7333621d8122
Kanchana Sondee <kangana512@gmail.com>
2021-05-05 21:41:44 +0700

    add demo page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
-import '../styles/globals.css'
 const Home = () => {
   return (
     <div className="relative bg-white overflow-hidden">
       <div className="max-w-7xl mx-auto">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <svg
+            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
             fill="currentColor"
-            aria-hidden="true"
-            preserveAspectRatio="none"
             viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
           >
-            <path d="M50 0L100 0 50 100 0 100z"></path>
+            <polygon points="50,0 100,0 50,100 0,100" />
           </svg>
 
           <div>
